6b7cf20c247b0c8096de5d4f607ee35240218756
Paulo Victor <paulo.tspi@gmail.com>
2023-03-29 16:32:04 -0300

    fix: button dog interactions and fix css to mobile devices

--- Js/index.js
+++ Js/index.js
@@ -1,13 +1,11 @@
 const openButton = document.querySelector('#open')
 const closeButton = document.querySelector('#close')
 const menu = document.querySelector('.brandAndMenu nav')
+const links = document.querySelector('.brandAndMenu ul li')
 
-openButton.addEventListener('click', () => {
-  CloseAndOpenMenu()
-})
-closeButton.addEventListener('click', () => {
-  CloseAndOpenMenu()
-})
+openButton.addEventListener('click', CloseAndOpenMenu)
+closeButton.addEventListener('click', CloseAndOpenMenu)
+links.addEventListener('click', CloseAndOpenMenu)
 
 function CloseAndOpenMenu() {
   openButton.classList.toggle('hide')
